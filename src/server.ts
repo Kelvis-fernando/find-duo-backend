ed56b0e7123d405d8a54980a81dfdf557f32bd3c
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-09-13 22:08:36 -0300

    added all rouutes and config in app the data base and orm prisma

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/ads', (req, res) => {
-    console.log('Acessou ads')
-    return res.json([
-        {id:1, name: 'Anuncio 1'},
-        {id:2, name: 'Anuncio 2'},
-        {id:3, name: 'Anuncio 3'},
-        {id:5, name: 'Anuncio 5'},
-    ])
+app.get("/games", (req, res) => {
+  res.json([]);
 });
 
-app.listen(3333)
+app.post("/ads", (req, res) => {
+  res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  const gameId = req.params.id;
+  return res.json([
+    { id: 1, name: gameId },
+    { id: 2, name: "Anuncio 2" },
+    { id: 3, name: "Anuncio 3" },
+    { id: 5, name: "Anuncio 5" },
+  ]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  const adId = req.params.id;
+  return res.json([{ id: 1, name: adId }]);
+});
+
+app.listen(3333);
