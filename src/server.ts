db150920001219ceaeadc790869dda32c1c42dc4
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-09-14 10:37:26 -0300

    Populated db and remove all mocks from routes in back-end, and added all responses from data base, making this responsible

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,70 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/games", (req, res) => {
-  res.json([]);
+app.get("/games", async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  res.json(games);
 });
 
 app.post("/ads", (req, res) => {
   res.status(201).json([]);
 });
 
-app.get("/games/:id/ads", (req, res) => {
+app.get("/games/:id/ads", async (req, res) => {
   const gameId = req.params.id;
-  return res.json([
-    { id: 1, name: gameId },
-    { id: 2, name: "Anuncio 2" },
-    { id: 3, name: "Anuncio 3" },
-    { id: 5, name: "Anuncio 5" },
-  ]);
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChanel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return res.json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekDays: ad.weekDays.split(","),
+      };
+    })
+  );
 });
 
-app.get("/ads/:id/discord", (req, res) => {
+app.get("/ads/:id/discord", async (req, res) => {
   const adId = req.params.id;
-  return res.json([{ id: 1, name: adId }]);
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return res.json({ discord: ad.discord});
 });
 
 app.listen(3333);
